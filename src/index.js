b18f788e0bddae46e3cb5bcea3a636c0c245a3cf
OxanaDanilova <oxanadanilova11@gmail.com>
2019-10-07 17:41:29 +0200

    feat: create multiply function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let arg1 = BigInt(first);
+  let arg2 = BigInt(second);
+  let res = BigInt(arg1*arg2);
+  return String(res);
+
 }
